861cd677fceebb624558d2b03f3ffd45090ddf90
peterthejohnston <peter.johnston.xc@gmail.com>
2016-07-06 11:17:37 -0700

    update settings.py for staticfiles on heroku

--- testsite/testsite/settings.py
+++ testsite/testsite/settings.py
@@ -121,6 +121,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = (
+    os.path.join (BASE_DIR, 'app', 'static'),
+)
+
 # Heroku setup
 import dj_database_url
 DATABASES['default'] = dj_database_url.config()
